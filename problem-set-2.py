38728a268adc70f5d6cbf41f4f481cb9f027825c
360mtucker <46805953+360mtucker@users.noreply.github.com>
2022-04-19 09:07:33 -0700

    These are the files for problem-set-2

---
+++ problem-set-2.py
@@ -0,0 +1,20 @@
+"""
+NTEC 361
+Date: mm/dd/yyyy
+Your Name
+Assignment Name: Problem Set No. 2
+Description: Simple coding problems to reinforce topics covered in Modules 0, 1, 2 and 3
+"""
+
+""" IPO template
+Input(s): 
+Process: 
+Output: 
+"""
+
+
+def main():
+    # Your code goes here. You must remove this line
+
+
+main()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# NTEC 361 - Problem Set 2
+
+This is the readme file for Problem Set 2
+
+See Canvas for the assignment details
